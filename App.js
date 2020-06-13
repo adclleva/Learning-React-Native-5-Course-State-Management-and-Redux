dda494f471712cd10cc9ed65700aea7b2dfc0425
adclleva <arvin.lleva@yahoo.com>
2020-06-12 21:38:50 -0700

    Added the fonts by utilizing the AppLoading API with the Font API from expo to have a loading screen until the fonts have been loaded

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  // this will return a promise
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -12,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const FiltersScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Favorite Screen!</Text>
+      <Text>The Filter Screen!</Text>
     </View>
   );
 };
