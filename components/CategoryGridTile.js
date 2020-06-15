c5f2b6d5ad74bac9fed4410f2de5951691d03db2
adclleva <arvin.lleva@yahoo.com>
2020-06-14 22:54:57 -0700

    Refactored the code for the CategoryGridTile in order to have the shadow show for both OS with the overflow parameter depending on the platform

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -42,7 +42,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden", // this will make sure that child items won't render outside this component
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible", // this will make sure that child items won't render outside this component
+    elevation: 5, // this is for android, bc the shadow properties only affect ios
   },
   container: {
     flex: 1,
@@ -51,7 +55,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3, // this is for android, bc the shadow properties only affect ios
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
