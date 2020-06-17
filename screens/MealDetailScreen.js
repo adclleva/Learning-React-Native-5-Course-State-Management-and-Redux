7b9a63249c993a009a9a23485ac351c05d8adc1f
adclleva <arvin.lleva@yahoo.com>
2020-06-16 22:33:13 -0700

    Added content and styling to the MealDetail Screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,24 +1,56 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Image,
+} from "react-native";
 import { MEALS } from "../data/dummy-data";
 import CustomHeaderButton from "../components/CustomHeaderButton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import DefaultText from "../components/DefaultText";
+
+const ListItem = (props) => {
+  // this component will be for the list items that we're generating
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
   const { navigation } = props;
   const mealId = navigation.getParam("mealId");
   const selectedMeal = MEALS.find((meal) => mealId === meal.id);
+  const {
+    duration,
+    complexity,
+    affordability,
+    imageUrl,
+    ingredients,
+    steps,
+  } = selectedMeal;
 
   return (
-    <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
-      <Button
-        title="Go Back to Categories"
-        onPress={() => {
-          navigation.popToTop();
-        }}
-      />
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: imageUrl }} />
+      <View style={styles.details}>
+        <DefaultText>{duration}m</DefaultText>
+        <DefaultText>{complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {ingredients.map((ingredient, index) => {
+        return <ListItem key={`${ingredient}${index}`}>{ingredient}</ListItem>;
+      })}
+      <Text style={styles.title}>Steps</Text>
+      {steps.map((step, index) => {
+        return <ListItem key={step}>{`${index + 1}. ${step}`}</ListItem>;
+      })}
+    </ScrollView>
   );
 };
 
@@ -47,9 +79,25 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  image: {
+    width: "100%",
+    height: 200, // we can use the dimensions api to configure this
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
   },
 });
