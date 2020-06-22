a3fe6dc4312b557a4969100ed7826b13a015dfd0
adclleva <arvin.lleva@yahoo.com>
2020-06-21 23:27:08 -0700

    Rendered a fallback text when there's not favorite meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import MealList from "../components/MealList";
 import CustomHeaderButton from "../components/CustomHeaderButton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import DefaultText from "../components/DefaultText";
 
 const FavoriteScreen = (props) => {
   const { navigation } = props;
 
   // this slices the meals state from the combined reducer and will get the meals state from it
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList navigation={navigation} listData={favMeals} />;
 };
@@ -29,3 +39,11 @@ FavoriteScreen.navigationOptions = (navData) => {
 };
 
 export default FavoriteScreen;
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
